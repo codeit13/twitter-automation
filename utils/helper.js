adc39e7c5ac24cf25728bb0348691015a6368983
Sumit Chauhan <cosmictunnel376@gmail.com>
2023-11-11 02:14:00 +0530

    Changed code screenshot endpoint

--- utils/helper.js
+++ utils/helper.js
@@ -98,7 +98,7 @@ async function tweetWithMedia(text, mediaPath) {
 
 const generateImageFromCode = async (code) => {
   const response = await axios.post(
-    "https://teknologi-umum-graphene.fly.dev/api",
+    "https://graphene.teknologiumum.com/api",
     {
       code: code,
       language: "javascript",
