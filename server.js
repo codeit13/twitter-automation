2e6b78fd62ebc62fb7f968fe8dd67a9e195dde3d
Sumit Chauhan <cosmictunnel376@gmail.com>
2023-11-11 00:58:37 +0530

    Added complete twiiter tip generator automation code

---
+++ server.js
@@ -0,0 +1,30 @@
+const {
+  tweetWithMedia,
+  generateImageFromCode,
+  generateTweetContent,
+  test,
+} = require("./utils/helper");
+
+const { CronJob } = require("cron");
+
+const tweetRandomTechTip = async () => {
+  try {
+    const { content, code } = await generateTweetContent();
+    const image = await generateImageFromCode(code);
+    await tweetWithMedia(content, image);
+
+    // await test();
+  } catch (e) {
+    console.log("Error posting tweet: ", e);
+  }
+};
+
+new CronJob(
+  "0 */5 * * *",
+  async function () {
+    await tweetRandomTechTip();
+  },
+  null,
+  true,
+  "Asia/Kolkata"
+);
---
+++ utils/helper.js
@@ -0,0 +1,243 @@
+require("dotenv").config();
+
+const OAuth = require("oauth-1.0a");
+const crypto = require("crypto");
+const FormData = require("form-data");
+const syncFs = require("fs");
+const fs = require("fs").promises;
+const axios = require("axios");
+const OpenAI = require("openai");
+
+const config = require("./config.json");
+
+// Your keys and tokens
+const CONSUMER_KEY = process.env.CONSUMER_KEY;
+const CONSUMER_SECRET = process.env.CONSUMER_SECRET;
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+const ACCESS_SECRET = process.env.ACCESS_SECRET;
+const BEARER_TOKEN = process.env.BEARER_TOKEN;
+const OAUTH_2_CLIENT_ID = process.env.OAUTH_2_CLIENT_ID;
+const OAUTH_2_CLIENT_SECRET = process.env.OAUTH_2_CLIENT_SECRET;
+
+// Initialize OAuth1.0a with your app's keys and hashing method
+const oauth = OAuth({
+  consumer: { key: CONSUMER_KEY, secret: CONSUMER_SECRET },
+  signature_method: "HMAC-SHA1",
+  hash_function(base_string, key) {
+    return crypto.createHmac("sha1", key).update(base_string).digest("base64");
+  },
+});
+
+// The user token
+const token = { key: ACCESS_TOKEN, secret: ACCESS_SECRET };
+
+function uploadMedia(mediaPath) {
+  return new Promise((resolve, reject) => {
+    // Prepare request data
+    const formData = new FormData();
+    formData.append("media", syncFs.createReadStream(mediaPath));
+    formData.append("media_category", "tweet_image");
+
+    // Prepare the OAuth Authorization header
+    const request_data = {
+      url: "https://upload.twitter.com/1.1/media/upload.json",
+      method: "POST",
+    };
+
+    const headers = oauth.toHeader(oauth.authorize(request_data, token));
+    Object.assign(headers, formData.getHeaders()); // This will add the Content-Type with boundary
+
+    // Make the request
+    axios
+      .post(request_data.url, formData, { headers })
+      .then((response) => {
+        // Resolve with media_id_string from the response
+        resolve(response.data.media_id_string);
+      })
+      .catch((error) => {
+        // Reject with error
+        reject(error.response ? error.response.data : error.message);
+      });
+  });
+}
+
+async function tweetWithMedia(text, mediaPath) {
+  try {
+    // Step 1: Upload the media and get the media ID
+    const mediaId = await uploadMedia(mediaPath);
+
+    // Step 2: Create a tweet with the text and media ID
+    const tweetData = {
+      text,
+      media: {
+        media_ids: [mediaId],
+      },
+    };
+
+    const request_data = {
+      url: "https://api.twitter.com/2/tweets",
+      method: "POST",
+    };
+    const headers = oauth.toHeader(oauth.authorize(request_data, token));
+
+    headers["Content-Type"] = "application/json";
+
+    const { data } = await axios.post(
+      "https://api.twitter.com/2/tweets",
+      tweetData,
+      {
+        headers,
+      }
+    );
+
+    console.log(`Tweet with id: ${data.data.id} posted successfully`);
+  } catch (error) {
+    console.log("Error:", error);
+  }
+}
+
+const generateImageFromCode = async (code) => {
+  const response = await axios.post(
+    "https://graphene.teknologiumum.com/api",
+    {
+      code: code,
+      language: "javascript",
+      theme: "slack-dark",
+      format: "png",
+      upscale: 4,
+      font: "hack",
+      border: { thickness: 40, radius: 7 },
+      showLineNumber: false,
+    },
+    {
+      headers: {
+        accept: "image/png",
+        "content-type": "application/json",
+      },
+      responseType: "arraybuffer", // Important to handle binary data
+    }
+  );
+
+  if (response.status !== 200) console.log("Failed to generate imag");
+
+  const imageBuffer = response.data; // This will be a Buffer since responseType is arraybuffer
+
+  const imagePath = `./assets/${Date.now()}.png`;
+
+  await fs.writeFile(imagePath, imageBuffer);
+
+  return imagePath;
+};
+
+const generateTweetContent = async () => {
+  const topic = config.topics[randomNumber(0, config.topics.length)];
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: `Generate a 2-3 line random tech-related, less known yet helpful life saviour tip on ${topic} and short code snippet demonstrating the tip. Return the response strictly in json format: { code: '', content: '' }. Make sure to beautify the code correctly, as prettier in vscode with proper indentations and include new lines wherever necessary. Also in code make sure every string after newline should not exceed max 35 chars limit.`,
+      },
+    ],
+    model: "gpt-3.5-turbo-1106",
+    response_format: { type: "json_object" },
+  });
+
+  const response = JSON.parse(chatCompletion.choices[0].message.content);
+
+  config.count += 1;
+
+  await fs.writeFile("./utils/config.json", JSON.stringify(config, null, 2));
+
+  response.content = `${bold(`Tech Tip #${config.count}`)}\n\n${bold(
+    topic
+  )}\n\n${response.content}`;
+
+  return { content: response.content, code: response.code };
+};
+
+function randomNumber(min, max) {
+  return parseInt(Math.random() * (max - min) + min);
+}
+
+function bold(inputString) {
+  const boldChars = {
+    A: "𝗔",
+    B: "𝗕",
+    C: "𝗖",
+    D: "𝗗",
+    E: "𝗘",
+    F: "𝗙",
+    G: "𝗚",
+    H: "𝗛",
+    I: "𝗜",
+    J: "𝗝",
+    K: "𝗞",
+    L: "𝗟",
+    M: "𝗠",
+    N: "𝗡",
+    O: "𝗢",
+    P: "𝗣",
+    Q: "𝗤",
+    R: "𝗥",
+    S: "𝗦",
+    T: "𝗧",
+    U: "𝗨",
+    V: "𝗩",
+    W: "𝗪",
+    X: "𝗫",
+    Y: "𝗬",
+    Z: "𝗭",
+    a: "𝗮",
+    b: "𝗯",
+    c: "𝗰",
+    d: "𝗱",
+    e: "𝗲",
+    f: "𝗳",
+    g: "𝗴",
+    h: "𝗵",
+    i: "𝗶",
+    j: "𝗷",
+    k: "𝗸",
+    l: "𝗹",
+    m: "𝗺",
+    n: "𝗻",
+    o: "𝗼",
+    p: "𝗽",
+    q: "𝗾",
+    r: "𝗿",
+    s: "𝘀",
+    t: "𝘁",
+    u: "𝘂",
+    v: "𝘃",
+    w: "𝘄",
+    x: "𝘅",
+    y: "𝘆",
+    z: "𝘇",
+  };
+
+  const boldString = inputString
+    .split("")
+    .map((char) => boldChars[char] || char)
+    .join("");
+  return boldString;
+}
+
+async function test() {
+  try {
+    console.log(bold("VueJS"));
+  } catch (e) {
+    console.log("Error: ", e);
+  }
+}
+
+module.exports = {
+  uploadMedia,
+  tweetWithMedia,
+  generateImageFromCode,
+  generateTweetContent,
+  test,
+};
